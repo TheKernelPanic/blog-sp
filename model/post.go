a0ba2a9951d40b4f66d375e457020c972cecef7c
DESKTOP-CFFKDUN\jorge <kernelpanic0x000007C4@protonmail.com>
2022-08-08 17:42:22 +0200

    Fix polyphormic relations.

--- model/post.go
+++ model/post.go
@@ -1,23 +1,37 @@
 package model
 
+type SectionType interface {
+	GetSection() Section
+	SetSection(section Section)
+}
+
 type Post struct {
 	ID          string    `gorm:"primary_key;type:varchar(36);default:uuid_generate_v4()" json:"id"`
-	Sections    []Section `gorm:"foreignKey:PostID;references:ID"`
+	Sections    []Section `gorm:"foreignKey:PostID;references:ID" json:"sections"`
 	Description string    `gorm:"type:varchar(255);not null" json:"description"`
 	Slug        string    `gorm:"type:varchar(255);not null" json:"slug"`
 	*TimestampFields
 }
 
 type Section struct {
-	ID           string `gorm:"primary_key;type:varchar(36);default:uuid_generate_v4()" json:"id"`
-	PostID       string `gorm:"type:varchar(36)" json:"post_id"`
-	Description  string `gorm:"type:varchar(255);not null" json:"description"`
-	MimeTypeID   int    `json:"mimetype_id"`
-	MimeTypeType string `json:"mimetype"`
+	ID          string `gorm:"primary_key;type:varchar(36);default:uuid_generate_v4()" json:"id"`
+	SectionID   int
+	SectionType string
+	PostID      string `gorm:"type:varchar(36)" json:"post_id"`
+	Mimetype    string `gorm:"type:varchar(255);not null" json:"mimetype"`
+	Sort        int    `gorm:"type:integer;not null"`
 }
 
 type HtmlSection struct {
 	ID      int
 	Content string  `gorm:"type:text" json:"content"`
-	Section Section `gorm:"polymorphic:MimeType;polymorphicValue:html"`
+	Section Section `gorm:"polymorphic:Section;polymorphicValue:html"`
+}
+
+func (htmlSection *HtmlSection) GetSection() Section {
+	return htmlSection.Section
+}
+
+func (htmlSection *HtmlSection) SetSection(section Section) {
+	htmlSection.Section = section
 }
